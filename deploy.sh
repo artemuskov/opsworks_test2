3b54be3f6db3b2c7a3b75320e61bd7b99a314a1f
Artem Uskov <blackphantom@ua.fm>
2018-06-20 09:48:24 +0300

    index.html commit 3

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 eval $(docker-machine env aws-nginx)
-docker run -d -p 8000:80 artemuskov/nginx-lua:latest
-docker ps
+docker pull artemuskov/nginx-lua
+docker container stop nginx-lua
+docker container rm nginx-lua
+docker run -d -p 8000:80 --name nginx-lua artemuskov/nginx-lua
 eval "$(docker-machine env -u)"
-
 
